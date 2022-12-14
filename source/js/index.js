f94c52b34c1b25d740f764b5bec5c3c000928439
Alena_Shuvaeva <minimarer@gmail.com>
2022-12-14 12:48:59 +0400

    Добавляет декоративные элементы

--- source/js/index.js
+++ source/js/index.js
@@ -1,7 +1,20 @@
 let button = document.querySelector('.main-header__button');
 let navigation = document.querySelector('.main-header__navigation');
+let header =  document.querySelector('.main-header');
 
-button.onclick = function() {
-  button.classList.toggle('main-header__button--is-open');
-  navigation.classList.toggle('main-header__navigation--is-open');
-};
+header.classList.remove('main-header--no-js');
+button.classList.add('main-header__button--closed');
+
+button.addEventListener('click', function () {
+  if (navigation.classList.contains('main-header__navigation--closed')) {
+    navigation.classList.remove('main-header__navigation--closed');
+    navigation.classList.add('main-header__navigation--is-open');
+    button.classList.remove('main-header__button--closed');
+    button.classList.add('main-header__button--is-open');
+  } else {
+    navigation.classList.add('main-header__navigation--closed');
+    navigation.classList.remove('main-header__navigation--is-open');
+    button.classList.remove('main-header__button--is-open');
+    button.classList.add('main-header__button--closed');
+  }
+});
